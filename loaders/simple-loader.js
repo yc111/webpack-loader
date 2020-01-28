5c1afd6618bc1b17bc908a43448e53bbce229ca5
yc111 <yinchuan_111@163.com>
2020-01-28 15:06:14 +0800

    write a simple-loader

---
+++ loaders/simple-loader.js
@@ -0,0 +1,5 @@
+
+module.exports = function loader (source) {
+    console.log('simple-loader is working');
+    return source;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          // 直接指明 loader 的绝对路径
+          use: path.resolve(__dirname, 'loaders/simple-loader')
+        }
+      ]
     }
 }
